bcc96a657066c92b282bacdaadc2508e45ebec3a
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-06-19 21:27:58 +0200

    CW - Code under pressure

---
+++ Pages/8. 29. You Can't Code Under Pressure #1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+/**
+ 
+ DESCRIPTION:
+ Code as fast as you can! You need to double the integer and return it.
+ 
+ **/
+
+func doubleInteger(_ num: Int) -> Int {
+
+  return num * 2
+}
+
+print(doubleInteger(4))
