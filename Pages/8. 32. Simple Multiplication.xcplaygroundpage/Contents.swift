27eaa985464e416774f9d95bc6210c98f8c8778c
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-06-20 12:46:17 +0200

    CW - Simple Multiply

---
+++ Pages/8. 32. Simple Multiplication.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+/**
+ 
+ DESCRIPTION:
+ This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+ 
+ **/
+
+func simpleMultiplication(_ num: Int) -> Int {
+    return num % 2 == 0 ? num * 8 : num * 9
+}
+
+print(simpleMultiplication(9))
