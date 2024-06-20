c51263c5b03868f7343eb568f60f612a62991c94
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-06-20 13:13:35 +0200

    CW - Quarter of the year

--- Pages/8. 33..xcplaygroundpage/Contents.swift
+++ Pages/8. 33..xcplaygroundpage/Contents.swift
@@ -1,7 +1,36 @@
-//: [Previous](@previous)
+/**
+ 
+ DESCRIPTION:
+ Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
-import Foundation
+ For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 
-var greeting = "Hello, playground"
+ Constraint:
+ 
+ 1 <= month <= 12
+ 
+ **/
 
-//: [Next](@next)
+func quarter(of month: Int) -> Int {
+    
+    switch month {
+    case 1, 2, 3: return 1
+    case 4, 5, 6: return 2
+    case 7, 8, 9: return 3
+    case 10, 11, 12: return 4
+    default: return 0
+    }
+}
+
+print(quarter(of: 6))
+
+// Alternative
+
+//func quarter(of month: Int) -> Int {
+//  switch month {
+//    case 0...3: return 1
+//    case 3...6: return 2
+//    case 6...9: return 3
+//    default : return 4
+//  }
+//}
