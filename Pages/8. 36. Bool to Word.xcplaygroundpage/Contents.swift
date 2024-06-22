07d2c52c0d5aaeb7480f2c44a81c4c086e00b151
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-06-22 14:00:23 +0200

    Bool to Word

---
+++ Pages/8. 36. Bool to Word.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+/**
+ 
+ DESCRIPTION:
+ Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ 
+ **/
+
+func boolToWord(_ bool: Bool) -> String {
+    bool ? "Yes" : "No"
+}
+
+print(boolToWord(false))
